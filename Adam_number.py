abf7e766a7fee5e7109f98631988564c1b6707ad
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-06-12 14:08:46 +0530

    n=input() m=n[::-1] sn=int(n)*int(n) sm=int(m)*int(m) if str(sn)==str(sm)[::-1]:     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,6 +1,8 @@
-n=int(input())
-x=int(str(n)[::-1])
-x1=x*x
-x1=int(str(x1)[::-1])
-if((n*n)==x1):print("True")
-else:print("False")
+n=input()
+m=n[::-1]
+sn=int(n)*int(n)
+sm=int(m)*int(m)
+if str(sn)==str(sm)[::-1]:
+    print("True")
+else:
+    print("False")
