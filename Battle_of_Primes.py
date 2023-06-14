e166841a0a0af3433c64a5cb6da2cc83fd4edc04
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-06-14 14:36:57 +0530

    def is_prime(n):     if n<=1:         return 0     for i in range(2,int(n**(0.5))+1):         if(n%i==0):return 0     return 1 n=int(input()) m=int(input()) s=n+m+1 while((n+m)):     if(is_prime(s)):print(s-(n+m));break;     else:s+=1

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,19 +1,12 @@
 def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-    
-def post_prime(n):
-    upper = n + 1
-    while True:
-        if is_prime(upper):
-            return upper
-        upper += 1
-
-n1=int(input())
-n2=int(input())
-ans=post_prime(n1+n2)
-print(ans-(n1+n2))
+    if n<=1:
+        return 0
+    for i in range(2,int(n**(0.5))+1):
+        if(n%i==0):return 0
+    return 1
+n=int(input())
+m=int(input())
+s=n+m+1
+while((n+m)):
+    if(is_prime(s)):print(s-(n+m));break;
+    else:s+=1
