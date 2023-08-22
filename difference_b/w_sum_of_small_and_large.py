762f3a6e9bde29a1ac878a502439ea6a3d94a6da
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-08-22 13:12:09 +0530

    s=input() w="" mn=0 mx=0 for i in s:     if i!=" ":         w+=i     if i==" ":         mn+=ord(min(w))         mx+=ord(max(w))         w="" else:     mn+=ord(min(w))     mx+=ord(max(w)) print(abs(mn-mx))

---
+++ difference_b/w_sum_of_small_and_large.py
@@ -0,0 +1,15 @@
+s=input()
+w=""
+mn=0
+mx=0
+for i in s:
+    if i!=" ":
+        w+=i
+    if i==" ":
+        mn+=ord(min(w))
+        mx+=ord(max(w))
+        w=""
+else:
+    mn+=ord(min(w))
+    mx+=ord(max(w))
+print(abs(mn-mx))
