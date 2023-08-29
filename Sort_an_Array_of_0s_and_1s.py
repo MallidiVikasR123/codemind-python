d5853cb64af91b45adbe7fd5fe79c7651f17cdf1
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-08-29 15:28:27 +0530

    n=int(input()) a=list(map(int,input().split())) z=a.count(0) o=a.count(1) r=[] for i in range(z):     r.append(0) for i in range(o):     r.append(1) print(*r)

--- Sort_an_Array_of_0s_and_1s.py
+++ Sort_an_Array_of_0s_and_1s.py
@@ -1,3 +1,10 @@
 n=int(input())
 a=list(map(int,input().split()))
-print(*sorted(a))
+z=a.count(0)
+o=a.count(1)
+r=[]
+for i in range(z):
+    r.append(0)
+for i in range(o):
+    r.append(1)
+print(*r)
