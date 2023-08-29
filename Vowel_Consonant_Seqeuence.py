d85016231b6966f09a2b413bcc814670f8b7a638
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-08-29 09:43:32 +0530

    s=input() a="" r="" for i in s:     if i not in "aeiouAEIOU":r+="C"     elif i in "aeiouAEIOU":r+="V" i=0 l=len(r) while(i<l):     if r[i]=="C":         a+="C"         while(i<l and r[i]!="V"): i+=1     else:         a+="V"         while(i<l and r[i]!="C"): i+=1 print(a)

--- Vowel_Consonant_Seqeuence.py
+++ Vowel_Consonant_Seqeuence.py
@@ -1,18 +1,16 @@
-r=input()
-s=''
-for i in r:
-    if i in "aeiouAEIOU":
-        s+='V'
-    else:s+='C'
-            
-ans=""
-n=len(s);i=0;
-while(i<n):
-    if(s[i]=='C'):
-        while(i<n and s[i]=='C'): i+=1;
-        ans+='C';
-        
+s=input()
+a=""
+r=""
+for i in s:
+    if i not in "aeiouAEIOU":r+="C"
+    elif i in "aeiouAEIOU":r+="V"
+i=0
+l=len(r)
+while(i<l):
+    if r[i]=="C":
+        a+="C"
+        while(i<l and r[i]!="V"): i+=1
     else:
-        while(i<n and s[i]=='V'): i+=1;
-        ans+='V';
-print(ans)
+        a+="V"
+        while(i<l and r[i]!="C"): i+=1
+print(a)
