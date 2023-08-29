b50d81d33cf0f685fb917ecac055c63c534d4ab2
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-08-29 15:15:06 +0530

    s=input() w="" r="" for i in range(len(s)-1,-1,-1):     if s[i]!=" ":         w+=s[i]     elif s[i]==" ":         r+=w[::-1]+" "         w="" else:     r+=w[::-1] print(r)

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,1 +1,12 @@
-print(input()[::-1])
+s=input()
+w=""
+r=""
+for i in range(len(s)-1,-1,-1):
+    if s[i]!=" ":
+        w+=s[i]
+    elif s[i]==" ":
+        r+=w[::-1]+" "
+        w=""
+else:
+    r+=w[::-1]
+print(r)
