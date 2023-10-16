478e188268076db144e154153b5e8c91dd1a9fa4
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-10-16 13:25:45 +0530

    for _ in range(int(input())):     n=int(input())     a=list(map(int,input().split()))     b=sorted(a)     if a==b:print("0")     else:print(abs(b[0]-b[-1]))

--- Optimal_Sorting.py
+++ Optimal_Sorting.py
@@ -1,8 +1,6 @@
-t=int(input())
-for i in range(t):
+for _ in range(int(input())):
     n=int(input())
-    l=list(map(int,input().split()))
-    if(sorted(l)==l):
-        print("0")
-    else:
-        print(max(l)-min(l))
+    a=list(map(int,input().split()))
+    b=sorted(a)
+    if a==b:print("0")
+    else:print(abs(b[0]-b[-1]))
